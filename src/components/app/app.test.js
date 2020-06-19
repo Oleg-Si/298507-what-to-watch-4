d866c532824abbdb461a9492af918124f2afb65f
OlegS <xwx18@mail.ru>
2020-06-19 17:36:24 +0300

    Обновляет тест компонента App

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -8,7 +8,28 @@ const promoFilmMock = {
   date: `2014`
 };
 
-const films = [`Fantastic Beasts: The Crimes of Grindelwald`, `The Grand Budapest Hotel`, `Bohemian Rhapsody`, `Macbeth`];
+const films = [
+  {
+    id: 0,
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    img: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
+  },
+  {
+    id: 1,
+    title: `Bohemian Rhapsody`,
+    img: `img/bohemian-rhapsody.jpg`
+  },
+  {
+    id: 2,
+    title: `Macbeth`,
+    img: `img/macbeth.jpg`
+  },
+  {
+    id: 3,
+    title: `Aviator`,
+    img: `img/aviator.jpg`
+  }
+];
 
 const onFilmCardTitleClick = () => {};
 
