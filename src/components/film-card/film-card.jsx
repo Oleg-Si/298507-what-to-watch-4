dc96141f21c274061f674b9fd681b53dbf084aba
OlegS <xwx18@mail.ru>
2020-06-19 17:10:15 +0300

    Рефакторинг компонента FilmCard

--- src/components/film-card/film-card.jsx
+++ src/components/film-card/film-card.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const FilmCard = (props) => {
-  const {title, onFilmCardTitleClick} = props;
+const IMG_SETTINGS = {
+  width: 280,
+  height: 175
+};
 
-  return <article className="small-movie-card catalog__movies-card">
-    <div className="small-movie-card__image">
-      <img src="img/macbeth.jpg" alt="Macbeth" width="280" height="175" />
-    </div>
-    <h3 className="small-movie-card__title">
-      <a className="small-movie-card__link" href="movie-page.html" onClick={onFilmCardTitleClick}>{title}</a>
-    </h3>
-  </article>;
+const FilmCard = (props) => {
+  const {film, onFilmCardMouseEnter, onFilmCardTitleClick} = props;
+  const {title, img} = film;
+
+  return (
+    <article className="small-movie-card catalog__movies-card" onMouseEnter={() => {
+      onFilmCardMouseEnter(film);
+    }}>
+      <div className="small-movie-card__image">
+        <img src={img} alt={title} width={IMG_SETTINGS.width} height={IMG_SETTINGS.height}/>
+      </div>
+      <h3 className="small-movie-card__title">
+        <a className="small-movie-card__link" href="movie-page.html" onClick={onFilmCardTitleClick}>{title}</a>
+      </h3>
+    </article>
+  );
 };
 
 FilmCard.propTypes = {
-  title: PropTypes.string.isRequired,
+  film: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired
+  }),
+  onFilmCardMouseEnter: PropTypes.func.isRequired,
   onFilmCardTitleClick: PropTypes.func.isRequired
 };
 
