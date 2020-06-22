e48b8a936d26e12be312bef65bd1d408bf6b8ab9
OlegS <xwx18@mail.ru>
2020-06-22 21:10:57 +0300

    Обновляет e2e тест компонента FilmCard

--- src/components/film-card/film-card.e2e.test.js
+++ src/components/film-card/film-card.e2e.test.js
@@ -9,8 +9,15 @@ Enzyme.configure({
 
 const film = {
   id: 0,
-  title: `Fantastic Beasts: The Crimes of Grindelwald`,
-  img: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
+  title: `Revenant`,
+  img: `img/revenant.jpg`,
+  genre: `Drama`,
+  rating: `8.5`,
+  releaseDate: 1552915226386,
+  ratingCount: 148,
+  description: [`In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave&aposs friend and protege.`, `Gustave prides himself on providing first-class service to the hotel&apos;s guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave&aposs lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.`],
+  director: [`Bill Murray`],
+  starring: [`Edward Norton`, `Jude Law`, `Willem Dafoe and other`, `Wes Andreson`]
 };
 
 const mockEvent = {
@@ -35,6 +42,7 @@ it(`Клик на заголовок вызывает коллбэк`, () => {
 
   // Обработчик был вызван 1 раз
   expect(onFilmCardTitleClick).toHaveBeenCalledTimes(1);
+  expect(onFilmCardTitleClick.mock.calls[0][0]).toMatchObject(film);
 });
 
 it(`При наведении на карточку фильма в обработчик попадает информация о фильме`, () => {
