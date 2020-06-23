2fdfc7363725fa17896c4c2d3aea27965a120cd5
OlegS <xwx18@mail.ru>
2020-06-23 14:09:06 +0300

    Добавляет моки для отрисовки /dev-film

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -9,6 +9,19 @@ const Screens = {
   FILM_PAGE: `FilmPage`
 };
 
+const mockFilm = {
+  id: 0,
+  title: `Revenant`,
+  img: `img/revenant.jpg`,
+  genre: `Drama`,
+  rating: `8.5`,
+  releaseDate: 1552915226386,
+  ratingCount: 148,
+  description: [`In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave&aposs friend and protege.`, `Gustave prides himself on providing first-class service to the hotel&apos;s guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave&aposs lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.`],
+  director: [`Bill Murray`],
+  starring: [`Edward Norton`, `Jude Law`, `Willem Dafoe and other`, `Wes Andreson`]
+};
+
 class App extends PureComponent {
   constructor(props) {
     super(props);
@@ -60,7 +73,7 @@ class App extends PureComponent {
             {this._renderApp()}
           </Route>
           <Route exact path="/dev-film">
-            <FilmPage />
+            <FilmPage activeFilm={mockFilm} />
           </Route>
         </Switch>
       </BrowserRouter>
