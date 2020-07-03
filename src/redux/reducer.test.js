863d8a300e8ab2c7e785ae5b2eb1240c35834c84
Oleg S <xwx18@mail.ru>
2020-07-03 22:35:35 +0300

    Добавляет тест reduce

---
+++ src/redux/reducer.test.js
@@ -0,0 +1,68 @@
+import {DEFAULT_GENRE} from '../constants';
+import films, {promoFilmMock, mockFilmsForTests} from '../mock/films';
+import {ActionType} from './action-type';
+import reducer from './reducer';
+
+const initialState = {
+  activeGenre: DEFAULT_GENRE,
+  films,
+  filteredFilmsByGenre: films,
+  promoFilmMock
+};
+
+describe(`Reducer работает корректно`, () => {
+  it(`Cоздается корректный initialState`, () => {
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  it(`Reducer меняет activeGenre на переданный`, () => {
+    expect(reducer({
+      activeGenre: DEFAULT_GENRE,
+      films,
+      filteredFilmsByGenre: films,
+      promoFilmMock
+    }, {
+      type: ActionType.CHANGE_GENRE,
+      payload: `Dramas`
+    })).toEqual({
+      activeGenre: `Dramas`,
+      films,
+      filteredFilmsByGenre: films,
+      promoFilmMock
+    });
+  });
+
+  it(`Reducer меняет filteredFilmsByGenre на отфильтрованный массив`, () => {
+    expect(reducer({
+      activeGenre: DEFAULT_GENRE,
+      films,
+      filteredFilmsByGenre: films,
+      promoFilmMock
+    }, {
+      type: ActionType.FILTER_FILMS_BY_GENRE,
+      payload: mockFilmsForTests
+    })).toEqual({
+      activeGenre: DEFAULT_GENRE,
+      films,
+      filteredFilmsByGenre: mockFilmsForTests,
+      promoFilmMock
+    });
+  });
+
+  it(`Reducer при ошибочном ActionType возвращает начальный стейт`, () => {
+    expect(reducer({
+      activeGenre: DEFAULT_GENRE,
+      films,
+      filteredFilmsByGenre: films,
+      promoFilmMock
+    }, {
+      type: null,
+      payload: mockFilmsForTests
+    })).toEqual({
+      activeGenre: DEFAULT_GENRE,
+      films,
+      filteredFilmsByGenre: films,
+      promoFilmMock
+    });
+  });
+});
