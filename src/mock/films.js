e102bb1cb2ecb85f173e2fa9010e9fc19eae94cb
Oleg S <xwx18@mail.ru>
2020-07-06 10:52:29 +0300

    Дополняет моки

--- src/mock/films.js
+++ src/mock/films.js
@@ -58,13 +58,77 @@ const getMockFilm = () => ({
   src: getRandomArrayItems([`https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4`, `https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4`, `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`, `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`, `https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4`], 1),
   rating: `${getRandomInt(0, 9)}.${getRandomInt(0, 9)}`,
   releaseDate: getRandomInt(1262293200000, 1552915226386),
+  runTime: getRandomInt(1262293200000, 1552915226386),
   ratingCount: getRandomInt(5, 250),
   description: getRandomArrayItems([`In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave&aposs friend and protege.`, `Gustave prides himself on providing first-class service to the hotel&apos;s guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave&aposs lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.`, `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra.`, `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`, `Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`], getRandomInt(2, 5)),
-  director: getRandomArrayItems([`Bill Murray`, `Edward Norton`, `Jude Law`, `Willem Dafoe and other`, `Wes Andreson`], 1),
-  starring: getRandomArrayItems([`Bill Murray`, `Edward Norton`, `Jude Law`, `Willem Dafoe and other`, `Wes Andreson`], getRandomInt(3, 5))
+  director: getRandomArrayItems([
+    `Bill Murray`,
+    `Edward Norton`,
+    `Jude Law`,
+    `Willem Dafoe`,
+    `Saoirse Ronan`,
+    `Tony Revoloru`,
+    `Tilda Swinton`,
+    `Tom Wilkinson`,
+    `Owen Wilkinson`,
+    `Adrien Brody`,
+    `Ralph Fiennes`,
+    `Jeff Goldblum`
+  ], 1),
+  starring: getRandomArrayItems([
+    `Bill Murray`,
+    `Edward Norton`,
+    `Jude Law`,
+    `Willem Dafoe`,
+    `Saoirse Ronan`,
+    `Tony Revoloru`,
+    `Tilda Swinton`,
+    `Tom Wilkinson`,
+    `Owen Wilkinson`,
+    `Adrien Brody`,
+    `Ralph Fiennes`,
+    `Jeff Goldblum`
+  ], getRandomInt(3, 12)),
+  reviews: getMockReviews(getRandomInt(3, 7))
 });
 
-const films = getFilms(FILM_COUNT);
+const getMockReviews = (count) => {
+  const allReviews = [];
+
+  for (let i = 0; i < count; i++) {
+    const element = getReviews();
+    allReviews.push(element);
+  }
+
+  return allReviews;
+};
+
+const getReviews = () => ({
+  author: getRandomArrayItems([
+    `Bill Murray`,
+    `Edward Norton`,
+    `Jude Law`,
+    `Willem Dafoe`,
+    `Saoirse Ronan`,
+    `Tony Revoloru`,
+    `Tilda Swinton`,
+    `Tom Wilkinson`,
+    `Owen Wilkinson`,
+    `Adrien Brody`,
+    `Ralph Fiennes`,
+    `Jeff Goldblum`
+  ], 1),
+  rating: `${getRandomInt(0, 9)}.${getRandomInt(0, 9)}`,
+  date: getRandomInt(1262293200000, 1552915226386),
+  comment: getRandomArrayItems([
+    `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
+    `The mannered, madcap proceedings are often delightful, occasionally silly, and here and there, gruesome and/or heartbreaking.`,
+    `Anderson's films are too precious for some, but for those of us willing to lose ourselves in them, they're a delight. "The Grand Budapest Hotel" is no different, except that he has added a hint of gravitas to the mix, improving the recipe.`,
+    `It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.`,
+    `I didn't find it amusing, and while I can appreciate the creativity, it's an hour and 40 minutes I wish I could take back.`,
+    `It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.`
+  ], 1)
+});
 
 const promoFilmMock = {
   title: `The Grand Budapest Hotel`,
@@ -127,6 +191,8 @@ const mockFilmsForTests = [
     starring: [`Edward Norton`, `Willem Dafoe and other`, `Wes Andreson`]
   }
 ];
+
+const films = getFilms(FILM_COUNT);
 
 export default films;
 export {promoFilmMock, mockFilmForTests, mockFilmsForTests};
