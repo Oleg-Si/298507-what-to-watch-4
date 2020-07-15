2b5631c7e32f8e2bce79c7734fd3b93936d4be4b
OlegS <xwx18@mail.ru>
2020-07-15 03:12:56 +0300

    Подключает api, redux-devtools-extension, middleware

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducer from './redux/reducer';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import createAPI from './api';
+
+const api = createAPI();
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    composeWithDevTools(applyMiddleware(thunk.withExtraArgument(api)))
 );
 
 ReactDOM.render(
