8f58077a0bb547d7a988be537167ab3cd7c825d7
OlegS <xwx18@mail.ru>
2020-07-16 22:30:39 +0300

    Выносит state компонента AddReview в хок

--- src/components/add-review/add-review.test.js
+++ src/components/add-review/add-review.test.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import {mockReviews} from '../../mock/films.js';
-import AddReview from './add-review';
+import {AddReview} from './add-review';
 
 it(`Проверяет снепшот компонента AddReview`, () => {
   const tree = renderer.create(
       <AddReview
         filmReviews={mockReviews}
+        isCorrectCommentLength={true}
         filmId={1}
         onSubmit={() => {}}
+        onTextareaChange={() => {}}
       />
   ).toJSON();
 
