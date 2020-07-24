56c739332bac9e2a89b86efef69e215bc846712b
OlegS <xwx18@mail.ru>
2020-07-24 11:54:19 +0300

    Обновляет импорт index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom';
 import App from './components/app/app';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
